123ee3934c877c0c3770a5112861828b102378f5
rubeeengm <rubeeengm@outlook.com>
2022-07-26 23:30:58 -0500

    + se agrega ejemplo de filtro mas complejo al consultar informacion

--- test_search_read.py
+++ test_search_read.py
@@ -1,13 +1,14 @@
 import xmlrpc.client
 from xmlrpc.client import Fault
 
-db = "test"
-username = "test@mail.com"
-password = ""
-url = "http://localhost:8076"
+db = 'test'
+username = 'test@mail.com'
+password = ''
+url = 'http://localhost:8076'
 
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 
+# autenticamos
 uid = common.authenticate(db, username, password, {})
 
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
@@ -20,14 +21,18 @@ info = models.execute_kw(
     , 'search_read'
     , [
         [
+            # '|'
+            # , [
+            #     'id', '=', 9
+            # ] ,
             [
-                'picking_type_id', '=', 1
+                'id', '=', 36
             ]
         ]
     ]
     , {
         'fields': [
-            'id', 'name', 'state', 'scheduled_date'
+            'id', 'name', 'state', 'scheduled_date', 'move_line_ids', 'move_lines'
         ]
     }
 )
